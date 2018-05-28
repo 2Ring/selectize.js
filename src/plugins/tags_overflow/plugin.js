4bfa8eba19822e4dd3f0b9eb89f7031ced9f314e
Vitez, Radko <Radko.Vitez@2ring.com>
2018-05-28 10:11:10 +0200

    update tags_overflow calculate

--- src/plugins/tags_overflow/plugin.js
+++ src/plugins/tags_overflow/plugin.js
@@ -19,24 +19,24 @@ Selectize.define('tags_overflow', function (options) {
         };
     })();
 
-    this.onChange = (function () {
+   var tagsUpdate = (function () {
         var original = self.onChange;
         return function (e) {
             var array = Array.prototype.slice.call(this.$control.children(':not(input):not(span)'));
             var controlInput = this.$control.children('input')[0];
             var actualWidth = 0;
             var isOutOfLine = false;
-            array.forEach(function(item) {
+            array.forEach(function (item) {
                 item.classList.remove("overflowed-item");
                 item.style.display = 'inline-block';
                 actualWidth += Math.abs(actualWidth - (item.offsetWidth + item.offsetLeft));
                 isOutOfLine = item.offsetTop > 10;
-                if (actualWidth > item.parentElement.clientWidth - controlInput.offsetWidth - 16 || isOutOfLine) {
+                if (actualWidth + controlInput.clientWidth + 16 >= item.parentElement.clientWidth || isOutOfLine) {
                     item.classList.add("overflowed-item");
                     item.style.display = 'none';
                 }
             });
-            if (actualWidth > this.$control[0].clientWidth - controlInput.clientWidth - 16 || isOutOfLine) {
+            if (actualWidth + controlInput.clientWidth + 16 > this.$control[0].clientWidth || isOutOfLine) {
                 this.overflow_indicator.style.display = 'inline-block';
 
             } else {
@@ -46,4 +46,8 @@ Selectize.define('tags_overflow', function (options) {
             return original.apply(this, arguments);
         };
     })();
+
+    this.onSearchChange = tagsUpdate;
+
+    this.onChange = tagsUpdate;
 });
