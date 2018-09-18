c6ff534365ad1b89bbf40a842a279f0ca5f29370
Radko Vitez <rad.vitez@hotmail.com>
2018-09-18 08:53:18 +0200

    fix (#4)

--- src/plugins/custom_width/plugin.js
+++ src/plugins/custom_width/plugin.js
@@ -6,14 +6,21 @@ Selectize.define('custom_width', function (options) {
             original.apply(this, arguments);
 
            if(self.settings.control_width) {
-               self.$control[0].style.maxWidth = self.settings.control_width;
-               self.$control[0].style.minWidth = self.settings.control_width;
+               self.$control[0].style.maxWidth = self.settings.control_width + 'px';
+               self.$control[0].style.minWidth = self.settings.control_width + 'px';
            }
             if (self.settings.dropdown_width) {
-               self.$dropdown[0].style.minWidth = self.settings.dropdown_width;
-               self.$dropdown[0].style.maxWidth = self.settings.dropdown_width;
+               self.$dropdown[0].style.minWidth = self.settings.dropdown_width + 'px';
+               self.$dropdown[0].style.maxWidth = self.settings.dropdown_width + 'px';
            }
 
+            self.on('item_add', function (value, item) {
+                if (self.settings.control_width) { 
+                    if(self.settings.mode === 'single') {
+                        item[0].style.maxWidth = (self.$control.width() - 6) + 'px';
+                    }
+                }
+            });
         };
     })();
 });
